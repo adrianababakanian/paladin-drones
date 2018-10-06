1ad02c69c8f750f7bb0d1c0293e0407c4b4eaba3
Adriana Babakanian <ababakanian@berkeley.edu>
2018-10-05 21:41:44 -0700

    main and content separation

--- paladin-drones/assets/index.js
+++ paladin-drones/assets/index.js
@@ -1,14 +1,24 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "500px";
-    //document.getElementById("main").style.marginRight = "500px";
-}
+$(document).ready(function(){
+  $sections = ['#mission', '#software', '#team', '#contact', '#press'];
+  $("#m_link").click(function(){
+       $section = ($(this).attr("href"));
+       $($section).toggleClass('show');
+   });
+});
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    //document.getElementById("main").style.marginRight = "0";
-}
+var lastScrollTop = 0;
+$(window).scroll(function(event){
+   var st = $(this).scrollTop();
+   if (st > 0){
+       // downscroll code
+       $('.main').addClass('shift');
+       console.log(st);
+   } else {
+      // upscroll code
+   }
+   lastScrollTop = st;
+});
+
 
 function toggleNav() {
   if ($('#nav-icon3').hasClass("open")) {
@@ -20,42 +30,15 @@ function toggleNav() {
   $('#menu').toggleClass('open');
 }
 
-  function colorReplace(findHexColor, replaceWith) {
-    // Convert rgb color strings to hex
-    function rgb2hex(rgb) {
-      if (/^#[0-9A-F]{6}$/i.test(rgb)) return rgb;
-      rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-      function hex(x) {
-        return ("0" + parseInt(x).toString(16)).slice(-2);
-      }
-      return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
-    }
-
-    // Select and run a map function on every tag
-    $('*').map(function(i, el) {
-      // Get the computed styles of each tag
-      var styles = window.getComputedStyle(el);
-
-      // Go through each computed style and search for "color"
-      Object.keys(styles).reduce(function(acc, k) {
-        var name = styles[k];
-        var value = styles.getPropertyValue(name);
-        if (value !== null && name.indexOf("color") >= 0) {
-          // Convert the rgb color to hex and compare with the target color
-          if (value.indexOf("rgb(") >= 0 && rgb2hex(value) === findHexColor) {
-            // Replace the color on this found color attribute
-            $(el).css(name, replaceWith);
-          }
-        }
-      });
-    });
-  }
-
  function toggleLights() {
-   // colorReplace("#1D1E21", "#F9FBFF");
-   // colorReplace("#F9FBFF", "#1D1E21");
    $('h1, h2, h3, h4, p, .logo-font, #nav-icon3 span').toggleClass('back');
 
-   $('.section, body, .fire, .sidenav, .sidenav a').toggleClass('light');
-   console.log("hello");
+   $('.section, body, .container, .fire, .sidenav, .sidenav a, p a, .section::-webkit-scrollbar-thumb, .placeholder, .gray').toggleClass('light');
+ }
+
+ function testing() {
+   console.log($(this))
+   var section = $(this).attr('href').substr(1);
+   console.log(section);
+   $('#mission').toggleClass('show');
  }
