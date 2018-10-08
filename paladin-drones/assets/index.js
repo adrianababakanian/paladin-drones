874ec680e8aa2876f73806e398f8e7300d9da089
Adriana Babakanian <ababakanian@berkeley.edu>
2018-10-07 17:32:46 -0700

    cleaner nav bar integration with videos

--- paladin-drones/assets/index.js
+++ paladin-drones/assets/index.js
@@ -11,7 +11,7 @@ function toggleNav() {
  function toggleLights() {
    $('h1, h2, h3, h4, p, .logo-font, #nav-icon3 span').toggleClass('back');
 
-   $('.section, body, .container, .fire, .sidenav, .sidenav a, .section::-webkit-scrollbar-thumb, .placeholder, .gray, .member, .column, .header-wrap, #drone_dark, #drone_light, .logo, .shade').toggleClass('light');
+   $('.section, body, .container, .fire, .sidenav, .sidenav a, .section::-webkit-scrollbar-thumb, .placeholder, .gray, .member, .column, .header-wrap, #drone_dark, #drone_light, .logo, .shade, .add_background').toggleClass('light');
  }
 
  function testing() {
@@ -27,6 +27,21 @@ function toggleNav() {
      $('#nav-icon3').toggleClass('open');
    }
  }
+
+$(document).ready(function(){
+  $(window).scroll(function() {
+      var scroll = $(window).scrollTop(); // how many pixels you've scrolled
+      var os = $('#home').offset().top; // pixels to the top of div1
+      var ht = $('#home').height(); // height of div1 in pixels
+      // if you've scrolled further than the top of div1 plus it's height
+      // change the color. either by adding a class or setting a css property
+      if(scroll > os + ht){
+        $('#header').addClass('add_background');
+      } else {
+        $('#header').removeClass('add_background');
+      }
+  });
+});
 
  $(document).ready(function(){
    $('a[href*="#"]')
